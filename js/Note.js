47ad016c5571722f071b5adc2f8b8990a305c59b
bernkaztel <bernkaztel@hotmail.com>
2018-04-02 10:07:18 -0500

    subiendo segundo alcance (función de editar)

--- js/Note.js
+++ js/Note.js
@@ -1,19 +1,53 @@
 var Note = React.createClass({
-    edit: function () {
-        
+    //The initial state of the note is not going to be editable
+    getInitialState: function() {
+        return {editing: false}
     },
-    remove: function () {
-        
+    //When you click on edit is going to set the state to be editable
+    edit: function() {
+        this.setState({editing: true});
     },
-    render: function () {
-        return <div className='note'>
-        <p>{this.props.children}</p>
-        <span>
-            <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"></button>
-            <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"></button>
-        </span>
-        </div>
+    //When you click save you can't edit anymore (state: not editable)
+    save: function() {
+        this.setState({editing: false});
+    },
+    remove: function() {
+        alert('removing note');
+    },
+    //The display render is the default state of the note (it has the icon to remove and edit)
+    renderDisplay: function() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit}
+                            className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove}
+                            className="btn btn-danger glyphicon glyphicon-trash"/>
+                </span>
+            </div>
+            );
+    },
+    //The display form is triggered if (editing: true) 
+    renderForm: function() {
+        return (
+            <div className="note">
+            <textarea defaultValue={this.props.children} 
+            className="form-control"></textarea>
+            <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+            </div>
+            )
+    },
+    //Depending on the state of the editing we call the (renderForm) or (renderDisplay)
+    render: function() {
+        if (this.state.editing) {
+            return this.renderForm();
+        }
+        else {
+            return this.renderDisplay();
+        }
     }
-})
+});
 
-React.render(<Note>Hello World</Note>, document.getElementById('react-container'));
+React.render(<Note>Hello World</Note>, 
+    document.getElementById('react-container'));
